a02496189230402adf735499975bcc81111b2b72
David Napravnik <d@nogare.cz>
2020-04-29 00:41:25 +0200

    main page ready to presentation

--- frontend/src/scenes/mainPage/mainPage.jsx
+++ frontend/src/scenes/mainPage/mainPage.jsx
@@ -1,8 +1,12 @@
 import React from "react"
 
 import Paper from '../../components/paper'
-import { 
+import { Pair } from '../../components/layout'
+import {
+	Button,
+	Chip,
 	CardContent,
+	FormControl,
 	Table,
 	TableBody,
 	TableCell,
@@ -11,7 +15,14 @@ import {
 	TableRow,
 	TextField,
 	Typography,
+	Slider,
+	InputLabel,
+	Input,
+	Divider,
+	InputAdornment,
+	Box,
 } from '@material-ui/core'
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
 import styles from './mainPage.module.scss'
 
@@ -23,34 +34,101 @@ export default class MainPageScene extends React.Component {
 		};		
 	}
 	render(){ return(
-		<div className={styles.MainPage}>
+		<div className={styles.mainPage}>
 			<div className={styles.LeftPanel}>
-				<Paper>
-					<TextField id="filled-basic" label="Search" variant="filled"/>
+				<Paper className={styles.searchField}>
+					<TextField 
+						variant="outlined"
+						placeholder="Search"
+						inputProps={{style: { textAlign: 'center' }}}
+					>
+						<Button className={styles.searchButton} variant="contained">find</Button>
+					</TextField>
+					
 				</Paper>
 				<Paper>
-					<CardContent>
-						<Typography gutterBottom variant="h5" component="h2">
-							Documents
-						</Typography>
-						<TextField id="outlined-basic" label="Type of document" variant="outlined" />
-						<TextField id="outlined-basic" label="Title" variant="outlined" />
-						<TextField id="outlined-basic" label="Location" variant="outlined" />
-						<TextField id="outlined-basic" label="Author" variant="outlined" />
-						<TextField id="outlined-basic" label="Year" variant="outlined" />			
+					<CardContent className={styles.gridFieldset}>
+						<TextField label="Type of document"/>
+						<TextField label="Title"/>
+						<TextField label="Author"/>
+						<TextField label="Location"/>	
 					</CardContent>
 				</Paper>
 				<Paper>
 					<CardContent>
-						<Typography gutterBottom variant="h5" component="h2">
-							Registers
+						<Typography id="non-linear-slider" gutterBottom>
+							Year range
 						</Typography>
-						<TextField id="outlined-basic" label="Location" variant="outlined" />
-						<TextField id="outlined-basic" label="Year" variant="outlined" />					
+						<Slider
+							valueLabelDisplay="auto"
+							aria-labelledby="range-slider"
+							getAriaValueText={(value)=>{return value}}
+							defaultValue={[2000, 2015]}
+							min={1980}
+							max={2020}
+							step={1}
+						/>
 					</CardContent>
 				</Paper>
+
+				<Pair>
+
+				<Paper>
+					<CardContent className={styles.gridChips}>		
+						<Typography>Keywords</Typography>
+						<TextField label="add keyword"/>
+						<div>
+							<Chip
+								label="childs"
+								variant="outlined"
+								onDelete={()=>{}}
+							/>
+							<Chip
+								label="swords"
+								variant="outlined"
+								onDelete={()=>{}}
+							/>
+							<Chip
+								label="ocean"
+								variant="outlined"
+								onDelete={()=>{}}
+							/>
+						</div>			
+					</CardContent>
+				</Paper>
+						
+				<Paper>
+					<CardContent className={styles.gridChips}>	
+						<Typography>Genre</Typography>
+						<TextField label="add genre"/>	
+						<div>
+							<Chip
+								label="horror"
+								variant="outlined"
+								clickable
+								onDelete={()=>{}}
+							/>
+							<Chip
+								label="sci-fi"
+								variant="outlined"
+								clickable
+								onDelete={()=>{}}
+							/>
+						</div>		
+					</CardContent>
+				</Paper>
+				</Pair>
 			</div>
 			<div className={styles.RightPanel}>
+				<Paper>
+					<iframe 
+						src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d152532.46197745937!2d15.54773112466256!3d50.737403035966864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2scz!4v1588113305104!5m2!1sen!2scz"
+						frameborder="0"
+						style={{border: "0", width:"100%", "min-height":"300px"}}
+						allowfullscreen
+					/>
+				</Paper>
+
 				<Paper>
 					<CardContent>
 						<Typography gutterBottom variant="h5" component="h2">
@@ -69,11 +147,11 @@ export default class MainPageScene extends React.Component {
 								</TableHead>
 								<TableBody>								
 									<TableRow key={0}>
-										<TableCell>{"Kniha"}</TableCell>
-										<TableCell>{"von. Sojka praskacka"}</TableCell>
-										<TableCell>{"Trantumberk"}</TableCell>
-										<TableCell>{"Vrchlabi"}</TableCell>
-										<TableCell>{1927}</TableCell>
+										<TableCell>{}</TableCell>
+										<TableCell>{}</TableCell>
+										<TableCell>{}</TableCell>
+										<TableCell>{}</TableCell>
+										<TableCell>{}</TableCell>
 									</TableRow>								
 								</TableBody>
 							</Table>
