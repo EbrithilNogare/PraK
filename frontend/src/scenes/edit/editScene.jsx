ddf7f16de59bf50f9f59b6dabb0f6992ab4f943e
David Napravnik <d@nogare.cz>
2020-04-29 01:02:12 +0200

    edit scene fixed and ready to present

--- frontend/src/scenes/edit/editScene.jsx
+++ frontend/src/scenes/edit/editScene.jsx
@@ -1,128 +1,151 @@
 import React from "react"
 
-import { 
+import { 	
 	Button,
+	ButtonGroup,
+	CardContent,
+	Checkbox,
+	FormControl,
+	InputLabel,
+	LinearProgress,
+	MenuItem,
+	Select,
 	Table,
 	TableBody,
 	TableCell,
 	TableContainer,
 	TableHead,
 	TableRow,
-	Checkbox,
+	TextField,
+	Typography,
 } from '@material-ui/core'
 
-import "./editScene.scss"
+import { Pair } from '../../components/layout'
+import Paper from '../../components/paper'
+
+import styles from './editScene.module.scss'
 
 class EditScene extends React.Component {
 	render(){
 		return(
-			<div className="EditScene">
-				<TableContainer>
-					<Table aria-label="simple table">
-						<TableHead>
-							<TableRow>
-								<TableCell>Sell</TableCell>
-								<TableCell>Type</TableCell>
-								<TableCell>Name</TableCell>
-								<TableCell>Aut</TableCell>
-								<TableCell>Editor</TableCell>
-							</TableRow>
-						</TableHead>
-						<TableBody>
-							
-							<TableRow>							
-								<TableCell>{<Checkbox></Checkbox>}</TableCell>	
-								<TableCell>{"book"}</TableCell>	
-								<TableCell>{"Krkk..."}</TableCell>	
-								<TableCell>{"muzemum vrc"}</TableCell>	
-								<TableCell>{"kmail broz"}</TableCell>
-							</TableRow>
-							
-						</TableBody>
-					</Table>
-				</TableContainer>
+			<div className={styles.editScene}>
+				<Paper>
+					<TableContainer>
+						<Table aria-label="simple table">
+							<TableHead>
+								<TableRow>
+									<TableCell>Sell</TableCell>
+									<TableCell>Type</TableCell>
+									<TableCell>Name</TableCell>
+									<TableCell>Aut</TableCell>
+									<TableCell>Editor</TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								
+								<TableRow>							
+									<TableCell>{<Checkbox></Checkbox>}</TableCell>	
+									<TableCell>{"book"}</TableCell>	
+									<TableCell>{"Krkk..."}</TableCell>	
+									<TableCell>{"muzemum vrc"}</TableCell>	
+									<TableCell>{"kmail broz"}</TableCell>
+								</TableRow>
+								
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</Paper>
 
-				<TableContainer>
-					<Table aria-label="simple table">
-						<TableBody>
-							
-						<TableRow>							
-								<TableCell>{"Type"}</TableCell>	
-								<TableCell>{"BOOK"}</TableCell>	
-							</TableRow>
-							
+				<Paper>
+					<TableContainer>
+						<Table aria-label="simple table">
+							<TableBody>
+								
 							<TableRow>							
-								<TableCell>{"Aut"}</TableCell>	
-								<TableCell>{"Muzeum"}</TableCell>	
-							</TableRow>
-							
-						</TableBody>
-					</Table>
-				</TableContainer>
+									<TableCell>{"Type"}</TableCell>	
+									<TableCell>{"BOOK"}</TableCell>	
+								</TableRow>
+								
+								<TableRow>							
+									<TableCell>{"Aut"}</TableCell>	
+									<TableCell>{"Muzeum"}</TableCell>	
+								</TableRow>
+								
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</Paper>
 
-				<TableContainer>
-					<Table aria-label="simple table">
-						<TableHead>
-							<TableRow>
-								<TableCell>Values</TableCell>
-							</TableRow>
-						</TableHead>
-						<TableBody>
-							
+				
+				<Paper>
+					<TableContainer>
+						<Table aria-label="simple table">
+							<TableHead>
+								<TableRow>
+									<TableCell>Values</TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								
+								<TableRow>							
+									<TableCell>{"+"}</TableCell>	
+								</TableRow>
+
+								<TableRow>	
+									<TableCell>{"Varnsdorf"}</TableCell>	
+								</TableRow>
+															
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</Paper>
+
+				<Paper>
+					<TableContainer>
+						<Table aria-label="simple table">
+							<TableHead>
+								<TableRow>
+									<TableCell>Sim</TableCell>
+									<TableCell>Type</TableCell>
+									<TableCell>Name</TableCell>
+									<TableCell>Aut</TableCell>
+									<TableCell>Editor</TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								
+								<TableRow>							
+									<TableCell>{"89%"}</TableCell>	
+									<TableCell>{"book"}</TableCell>	
+									<TableCell>{"Krkk..."}</TableCell>	
+									<TableCell>{"muzemum vrc"}</TableCell>	
+									<TableCell>{"kmail broz"}</TableCell>
+								</TableRow>
+								
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</Paper>
+
+				<Paper>
+					<TableContainer>
+						<Table aria-label="simple table">
+							<TableBody>
+								
 							<TableRow>							
-								<TableCell>{"+"}</TableCell>	
-							</TableRow>
-
-							<TableRow>	
-								<TableCell>{"Varnsdorf"}</TableCell>	
-							</TableRow>
-														
-						</TableBody>
-					</Table>
-				</TableContainer>
-
-				<TableContainer>
-					<Table aria-label="simple table">
-						<TableHead>
-							<TableRow>
-								<TableCell>Sim</TableCell>
-								<TableCell>Type</TableCell>
-								<TableCell>Name</TableCell>
-								<TableCell>Aut</TableCell>
-								<TableCell>Editor</TableCell>
-							</TableRow>
-						</TableHead>
-						<TableBody>
-							
-							<TableRow>							
-								<TableCell>{"89%"}</TableCell>	
-								<TableCell>{"book"}</TableCell>	
-								<TableCell>{"Krkk..."}</TableCell>	
-								<TableCell>{"muzemum vrc"}</TableCell>	
-								<TableCell>{"kmail broz"}</TableCell>
-							</TableRow>
-							
-						</TableBody>
-					</Table>
-				</TableContainer>
-
-				<TableContainer>
-					<Table aria-label="simple table">
-						<TableBody>
-							
-						<TableRow>							
-								<TableCell>{"Metadata"}</TableCell>	
-								<TableCell>{"filters"}</TableCell>	
-							</TableRow>
-							
-							<TableRow>							
-								<TableCell>{"Aut"}</TableCell>	
-								<TableCell>{"trantms"}</TableCell>	
-							</TableRow>
-							
-						</TableBody>
-					</Table>
-				</TableContainer>
+									<TableCell>{"Metadata"}</TableCell>	
+									<TableCell>{"filters"}</TableCell>	
+								</TableRow>
+								
+								<TableRow>							
+									<TableCell>{"Aut"}</TableCell>	
+									<TableCell>{"trantms"}</TableCell>	
+								</TableRow>
+								
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</Paper>
 
 				<Button variant="contained" color="primary">Send</Button>	
 
