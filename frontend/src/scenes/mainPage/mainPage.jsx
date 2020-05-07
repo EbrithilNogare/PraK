9dd02dfbda76b56340bf95cc3c71eaa95510d2f1
David Napravnik <d@nogare.cz>
2020-05-07 23:54:13 +0200

    domain hotfix

--- frontend/src/scenes/mainPage/mainPage.jsx
+++ frontend/src/scenes/mainPage/mainPage.jsx
@@ -53,19 +53,19 @@ export default class MainPageScene extends React.Component {
 				<Paper>
 					<div className={styles.leftInfo}>
 						<h1>O projektu PraK</h1>		
-						<img src="images/logo.png" alt="logo"/>							
+						<img src="prak/images/logo.png" alt="logo"/>							
 						{lorem.generateParagraphs(3)}
 					</div>
 					
 					<div className={styles.rightInfo}>
 						<h1>Jak PraK používat</h1>	
 						{lorem.generateParagraphs(3)}
-						<img src="images/devices.png" alt="devices"/>	
+						<img src="prak/images/devices.png" alt="devices"/>	
 					</div>
 					
 					<div className={styles.leftInfo}>
 						<h1>Pro koho je Prak určen</h1>	
-						<img src="images/students.png" alt="students"/>			
+						<img src="prak/images/students.png" alt="students"/>			
 						{lorem.generateParagraphs(3)}
 					</div>
 				</Paper>
