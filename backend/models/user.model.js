3c37e2f6362b3d849e345ea521e72d45aa233073
David Napravnik <d@nogare.cz>
2020-08-03 21:46:55 +0200

    typo

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -18,4 +18,4 @@ const schema = mongoose.Schema({
 })
 
 schema.plugin(uniqueValidator)
-module.exports = mongoose.model('users', userSchema, 'users')
+module.exports = mongoose.model('users', schema, 'users')
