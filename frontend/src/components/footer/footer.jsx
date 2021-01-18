6a67611aa2db83f42dc9ab042984fb4f410c8495
David Napravnik <d@nogare.cz>
2021-01-18 03:19:39 +0100

    bug try fix

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -42,8 +42,11 @@ class Footer extends React.Component {
 					<hr/>
 					<ul>
 						<li> <NavLink to="/prak/search">Vyhledávací rozhraní</NavLink> </li>
-						{console.log(this.props.cookies.get("permission"), this.props.cookies.get("permission") & 1, this.props.cookies.get("permission") & 2)}
+					</ul>
+					<ul>
 						{(this.props.cookies.get("permission") & 2) > 0 && <li> <NavLink to="/prak/upload">Nahrát soubor</NavLink> </li>}
+					</ul>
+					<ul>
 						{(this.props.cookies.get("permission") & 1) > 0 && <li> <NavLink to="/prak/admin">Admin</NavLink> </li>}
 					</ul>
 				</div>
