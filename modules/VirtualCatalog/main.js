c0a1841be249d44e59085ef92ba4797a45810bf3
David Napravnik <d@nogare.cz>
2021-12-23 01:27:59 +0100

    virtual catalog done

--- modules/VirtualCatalog/main.js
+++ modules/VirtualCatalog/main.js
@@ -1,19 +1,28 @@
 window.addEventListener("load", () => {
 	document.getElementById("leftButton" ).addEventListener("click", goLeft);
 	document.getElementById("rightButton").addEventListener("click", goRight);
+	
+	document.body.addEventListener("keydown", e => {
+		switch(e.key){
+			case "ArrowRight": goRight(); break;
+			case "ArrowLeft": goLeft(); break;
+		}
+	});
 
-	createNewPage("prevL", getPath(currentPage-2));
-	createNewPage("prevR", getPath(currentPage-1));
-	createNewPage("curnL", getPath(currentPage));
-	createNewPage("curnR", getPath(currentPage+1));
-	createNewPage("nextL", getPath(currentPage+2));
-	createNewPage("nextR", getPath(currentPage+3));
+	createNewPage("prevL", getPath((PAGE_COUNT + currentPage - 2) % PAGE_COUNT));
+	createNewPage("prevR", getPath((PAGE_COUNT + currentPage - 1) % PAGE_COUNT));
+	createNewPage("curnL", getPath((PAGE_COUNT + currentPage) % PAGE_COUNT));
+	createNewPage("curnR", getPath((PAGE_COUNT + currentPage + 1) % PAGE_COUNT));
+	createNewPage("nextL", getPath((PAGE_COUNT + currentPage + 2) % PAGE_COUNT));
+	createNewPage("nextR", getPath((PAGE_COUNT + currentPage + 3) % PAGE_COUNT));
 })
+
+const PAGE_COUNT = 358
 
 /**
  * left page, for right one add 1
  */
-let currentPage = 103;
+let currentPage = -1;
 
 function goLeft() {
 	let prevL = document.getElementById("prevL");
@@ -23,7 +32,7 @@ function goLeft() {
 	let curnL = document.getElementById("curnL");
 	let curnR = document.getElementById("curnR");
 
-	currentPage -= 2;
+	currentPage = (PAGE_COUNT + currentPage - 2) % PAGE_COUNT;
 
 	prevL.id = "curnL";
 	prevR.id = "curnR";
@@ -31,8 +40,8 @@ function goLeft() {
 	curnR.id = "nextR";
 	nextL.remove();
 	nextR.remove();
-	createNewPage("prevL", getPath(currentPage - 2));
-	createNewPage("prevR", getPath(currentPage - 1));
+	createNewPage("prevL", getPath(currentPage - 2), false);
+	createNewPage("prevR", getPath(currentPage - 1), false);
 }
 
 function goRight() {
@@ -43,7 +52,7 @@ function goRight() {
 	let curnL = document.getElementById("curnL");
 	let curnR = document.getElementById("curnR");
 
-	currentPage += 2;
+	currentPage = (PAGE_COUNT + currentPage + 2) % PAGE_COUNT;
 
 	nextL.id = "curnL";
 	nextR.id = "curnR";
@@ -51,22 +60,26 @@ function goRight() {
 	curnR.id = "prevR";
 	prevL.remove();
 	prevR.remove();
-	createNewPage("nextR", getPath(currentPage + 3));
-	createNewPage("nextL", getPath(currentPage + 2));
+	createNewPage("nextR", getPath(currentPage + 3), false);
+	createNewPage("nextL", getPath(currentPage + 2), false);
 }
 
 function getPath(img){
 	return `catalog/page (${img}).webp`;
 }
 
-function createNewPage(id, src){
+function createNewPage(id, src, now = true){
 	let root = document.getElementById("root");
 	let element = document.createElement("img");
 
 	element.id = id;
 	element.classList.add("page");
-	element.setAttribute("src", src);
+	element.setAttribute("alt", src);
+	
+	if(now)
+		element.setAttribute("src", src);
+	else
+		setTimeout(() => { element.setAttribute("src", src) }, 2000);
 
-	console.log(element, src);
 	root.appendChild(element);
 }
